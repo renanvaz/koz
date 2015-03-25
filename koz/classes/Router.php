b4bcdd4e48cd1c23d43bddcb25fc7176155d194e
Renan Vaz <renan.c.vaz@gmail.com>
2015-03-25 20:44:33 -0300

    Working...

--- koz/classes/Router.php
+++ koz/classes/Router.php
@@ -8,17 +8,35 @@ class Router {
     static private $_routes = [];
 
     /**
-     * Add a new route to math URI
+     * Set a new route to math URI
      *
      * @param string $name The alias of URI
      * @param string $route The URI mather
      * @param array $defaults The defaults values of params variables
      * @param: array $rules - Regex rules for match acceptable values for params on URI
      *
-     * @example Router::add('default', ':controller/:action(/:id)', ['controller' => 'teste', 'action' => 'index'], ['id' => '[0-9]+']);
+     * @example Router::set('default', ':controller/:action(/:id)', ['controller' => 'teste', 'action' => 'index'], ['id' => '[0-9]+']);
      */
+    static public function set($name, $route, array $defaults = [], array $rules = [])
+    {
+        $defaultRegex = self::REGEX_VALID_CHARACTERS;
 
-    static public function add ($name, $route, array $defaults = [], array $rules = []) {
+        self::$_routes[$name] = [
+            'regex'    => preg_replace_callback('!:('.$defaultRegex.')!', function($matches) use ($rules, $defaultRegex) { return '(?P<'.$matches[1].'>'.(\Helpers\Arr::get($rules, $matches[1], $defaultRegex)).')'; }, preg_replace('!\)!', ')?', $route)),
+            'defaults' => $defaults,
+        ];
+    }
+
+    /**
+     * Get the route URI
+     *
+     * @param string $name The alias of URI
+     * @param array $defaults The defaults values of params variables
+     *
+     * @example Route::get('default', ['controller' => 'teste', 'action' => 'index']);
+     */
+    static public function get($name, $route, array $defaults = [], array $rules = [])
+    {
         $defaultRegex = self::REGEX_VALID_CHARACTERS;
 
         self::$_routes[$name] = [
@@ -34,8 +52,8 @@ class Router {
      *
      * @example Router::parse('uri/segment');
      */
-
-    static public function parse ($uri) {
+    static public function parse ($uri)
+    {
         foreach (self::$_routes as $name => $data) {
             if (preg_match('!^'.$data['regex'].'$!', $uri, $matches)) {
                 foreach ($matches as $key => $value) {
